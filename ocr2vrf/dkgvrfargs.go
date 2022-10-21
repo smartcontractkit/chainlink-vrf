aef9beb5fcbfc8c47ef939b31da2e4971285480c
jinhoonbang <jin.bang@smartcontract.com>
2022-10-21 07:59:54 -0700

    update (#17)

--- ocr2vrf/dkgvrfargs.go
+++ ocr2vrf/dkgvrfargs.go
@@ -56,4 +56,7 @@ type DKGVRFArgs struct {
 	Esk   dkg_contract.EncryptionSecretKey
 	Ssk   dkg_contract.SigningSecretKey
 	KeyID dkg_contract.KeyID
+
+	DKGReportingPluginFactoryDecorator func(factory types.ReportingPluginFactory) types.ReportingPluginFactory
+	VRFReportingPluginFactoryDecorator func(factory types.ReportingPluginFactory) types.ReportingPluginFactory
 }
